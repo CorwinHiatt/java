af12f64487cb3bd8f0071117cff5ffed6a91e598
Corwin Hiatt <corwinhiatt@yahoo.com>
2023-05-29 19:29:32 -0400

    just horsing around

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,29 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		// 1. Create a new instance of the class
+		// 2. Call the method
+		// 3. Print the result
+		// 4. Run the application
+		// 5. Check the result
+		// 6. Commit the changes
+		// 7. Push the changes
 	}
 
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+		// 1. Create a new instance of the class
+		// 2. Call the method
+		// 3. Print the result
+		// 4. Run the application
+		// 5. Check the result
+		// 6. Commit the changes
+		// 7. Push the changes
+	}
+
+
+	
 }
